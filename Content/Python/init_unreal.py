129fc5373388a1b8739963b5c8ca47957f9aa2f5
Xavi Anadon <xvanadon@gmail.com>
2022-07-28 08:53:31 +0300

    Fixing the widget nodes

--- Content/Python/init_unreal.py
+++ Content/Python/init_unreal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fe2bd772c6a46fc9cb45c58f0d130504c7f93c5d386b73e1bb3b35d667bf5d5
-size 1249
+oid sha256:2056bd6c1c1a27bde5c10c7e2b4af6c81506edb407c63cba52888348184c6e52
+size 1251
